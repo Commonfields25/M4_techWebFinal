783e1c0ac11c94ef37450cf277db797e0564c804
Commonfields25 <noe.hadi@gmail.com>
2026-01-22 17:40:05 +0100

    Update home-resources.js

--- wwwroot/js/pages/home-resources.js
+++ wwwroot/js/pages/home-resources.js
@@ -1,18 +1,32 @@
 function resourceManager() {
     return {
-        favorites: JSON.parse(localStorage.getItem('favorites') || '[]'),
+        favorites: [],
 
+        /* Initialisation du composant */
+        init() {
+            const stored = localStorage.getItem('favorites');
+            this.favorites = stored ? JSON.parse(stored) : [];
+        },
+
+        /* Ajouter / retirer un favori */
         toggleFavorite(id) {
             if (this.isFavorite(id)) {
                 this.favorites = this.favorites.filter(favId => favId !== id);
             } else {
                 this.favorites.push(id);
             }
-            localStorage.setItem('favorites', JSON.stringify(this.favorites));
+
+            this.persist();
         },
 
+        /* Vérifier si une ressource est en favori */
         isFavorite(id) {
             return this.favorites.includes(id);
+        },
+
+        /* Sauvegarde centralisée */
+        persist() {
+            localStorage.setItem('favorites', JSON.stringify(this.favorites));
         }
-    }
+    };
 }
